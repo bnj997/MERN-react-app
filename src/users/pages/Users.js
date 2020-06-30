3ca17975fd45f4b99c006c81c501a8c718dc8ba5
bnj997 <bnj997@gmail.com>
2020-06-30 17:03:30 +1000

    Added more components for UI of Users

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 function Users() {
-  return <h2> Users Works! </h2>
+  const DUMMY_USERS = [
+    {
+      id: "u1",
+      name: "meepo",
+      image:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQGFl-WcfVNaKw/profile-displayphoto-shrink_200_200/0?e=1595462400&v=beta&t=72jo394Yu130e4xplsWmA78aMGC8zrm_F0hWDHPeReg",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={DUMMY_USERS} />
 }
 
 export default Users
