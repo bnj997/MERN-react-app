db605bdde1471ef60d2e3354269c4f4f163fbaa2
bnj997 <bnj997@gmail.com>
2020-06-30 18:17:06 +1000

    Added side nav bar functionality

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 
 function MainNavigation(props) {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  function openDrawer() {
+    setDrawerIsOpen(true);
+  }
+
+  function closeDrawer() {
+    setDrawerIsOpen(false);
+  }
+
+
   return (
-    <MainHeader className="main-navigation__menu-btn">
-      <button>
-        <span/>
-        <span/>
-        <span/>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/"> Your Places </Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      //Backdrop is a fullscreen background that sits behind the drawer
+      //Aim is that if background clicked, it will close drawer
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} /> }
+      <SideDrawer show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span/>
+          <span/>
+          <span/>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/"> Your Places </Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
