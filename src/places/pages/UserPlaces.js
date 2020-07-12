9bca7076add02ebc0e5c5b59f24aa907672f90fb
bnj997 <bnj997@gmail.com>
2020-07-12 15:59:26 +1000

    Added loading and error modal to user places

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 function UserPlaces() {
-  const DUMMY_PLACES = [
-    {
-      id: "p1",
-      title: "The title",
-      description: "The place",
-      imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQGFl-WcfVNaKw/profile-displayphoto-shrink_200_200/0?e=1595462400&v=beta&t=72jo394Yu130e4xplsWmA78aMGC8zrm_F0hWDHPeReg",
-      address: "20 W 34th St, New York, NY 10001, United States",
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
-    },
-    {
-      id: "p2",
-      title: "The title2",
-      description: "The place2",
-      imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQGFl-WcfVNaKw/profile-displayphoto-shrink_200_200/0?e=1595462400&v=beta&t=72jo394Yu130e4xplsWmA78aMGC8zrm_F0hWDHPeReg",
-      address: "20 W 34th St, New York, NY 10001, United States",
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u2'
-    }
-  ]
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const {isLoading, error, sendRequest, clearError} = useHttpClient();
 
-  // useParams() stores the dynamic segments set by parent class
+  //useParams() stores the dynamic segments set by parent class
   //In this case, in App.js,  our route path is "/:userId/places" meaning when we access this UserPlaces.js page, we can store the userId
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
+  //Send request when this component renders but not rerendered
+  useEffect(() => {
+    //users-controller in backend - res.json({places: places.map(place => place.toObject({getters: true})) });
+    async function fetchPlaces() {
+      try {
+        //users-route in backend - router.get('/user/:uid', placesControllers.getPlacesByUserId );
+        //Just a GETTER so no need for other params dictated by sendRequest
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        //we are receiving an array called "places" which is sent from users-controller in backend. so use responseData.places to access
+        setLoadedPlaces(responseData.places)
+      } catch(err) {}
+    }
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+
+  //note that empty arrays of loadedplaces are handled by PlaceList
   return (
-    <PlaceList items={loadedPlaces} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
   )
 }
 
